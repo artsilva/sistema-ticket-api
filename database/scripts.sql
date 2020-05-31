975eba0fd38cc9f53e16c54e95132e5fb287263b
Arturo Silva <arturo.silva@trebol-it.com>
2020-05-31 16:20:06 -0400

    Se actualizan script iniciles de db

--- database/scripts.sql
+++ database/scripts.sql
@@ -1,10 +1,24 @@
-CREATE DATABASE prueba;
+CREATE DATABASE ticket;
 
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(40),
-    email TEXT
+CREATE SCHEMA IF NOT EXISTS security;
+CREATE SCHEMA IF NOT EXISTS general;
+
+DROP TABLE IF EXISTS security.roles;
+CREATE TABLE security.roles(
+    "id" INTEGER PRIMARY KEY,
+    "role" VARCHAR(20)
 );
+INSERT INTO security.roles(id, role) VALUES (1, 'administrador'), (2, 'soporte');
 
-INSERT INTO users(name, email) 
-    VALUES ('joe','example@a.com'), ('ryan', 'asdf@gmail.c')
+DROP TABLE IF EXISTS security.users;
+CREATE TABLE security.users(
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(40),
+    "last_name_f" VARCHAR(40),
+    "last_name_m" VARCHAR(40),
+    "rut" VARCHAR(10),
+    "email" TEXT,
+    "role_id" INTEGER REFERENCES security.roles(id),
+    "user" VARCHAR(20),
+    "password" VARCHAR(20)
+);
